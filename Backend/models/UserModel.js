be45d39a84ca70033485c1f98b06bac0dd77cecf
amanprajapati3 <156068199+amanprajapati3@users.noreply.github.com>
2025-06-30 16:04:10 +0530

    testcase2

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const User_schema = new mongoose.Schema({
     _id: { type: String, required: true },
-    Username: { type: String, required: true },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     image: { type: String, required: true },
     role: { type: String, enum: ["user", "hotelowner"], default: "user" },
